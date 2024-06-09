03d74c14e963ce8aa9f527916109d1f2dfe9a866
Pranav RK <pranavrk7@gmail.com>
2024-06-09 18:41:17 +0530

    feat: add oil.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,6 +6,28 @@ return {
     "vim-pandoc/vim-pandoc-syntax",
   },
   {
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        win_options = {
+          wrap = false,
+          signcolumn = "no",
+          cursorcolumn = false,
+          foldcolumn = "0",
+          spell = false,
+          list = false,
+          conceallevel = 3,
+          concealcursor = "nvic",
+        },
+      })
+    end,
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+      { "<leader>-", "<cmd>require('oil').toggle_float()<cr>", desc = "Open parent directory" },
+    },
+  },
+  {
     "mbbill/undotree",
     keys = {
       {
